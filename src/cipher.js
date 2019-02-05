e8165f708503886da3a9b7b86cc912dba8f66748
pamelazoe <pmlzoee@gmail.com>
2019-02-05 10:21:01 -0600

    Se editaron comentarios para mejorar explicación paso por paso y se incluyó "test"

--- src/cipher.js
+++ src/cipher.js
@@ -1,137 +1,66 @@
- /*window.cipher = {
-   encode: (offset, string)=> {
-
-     let plainString = [];
-
-     for(let i=0; i < string.length; i++){
-         string[i].charCodeAt();
-         let positionEncode =(string[i].charCodeAt() - 65 + offset)%26 + 65;
-         let asciiString= String.fromCharCode(positionEncode);
-         plainString.push(asciiString);
-
-     }
-     let cipherString = plainString.join('');
-     return cipherString;
-   },
-
-   decode: (offset, string)=> {
-
-     let plainString = [];
-
-     for(let i=0; i < string.length; i++){
-         string[i].charCodeAt();
-         let positionDecode = (string[i].charCodeAt() + 65 - offset)%26 + 65;
-         let asciiString= String.fromCharCode(positionDecode);
-         plainString.push(asciiString);
-
-     }
-     let cipherString = plainString.join('');
-     return cipherString;
-   }
-   
- };
- */
-
-// window.cipher = {
-//   encode: (offset, texto)=> {
-//     let encrypt="";
-//     let temp = 0;
-//     
-
-//     //Repasa el string caracter por caracter
-//     for (let i=0; i<texto.length; i++){
-// 		temp=texto.charCodeAt(i);
-// 		if ((temp >= 65) && (temp <= 90)){
-// 			temp = (((temp - 65 + offset) % 26) + 65);
-// 		}
-// 		// Formula para los cambios de posición hacia la derecha de letras minúsculas
-// 		else if ((temp >= 97) && (temp <= 122)){
-// 			temp = (((temp - 97 + offset) % 26) + 97);
-// 		}
-//     // Agregar espacio: el caracter 32 en ASCII es equivalente a espacio
-// 		else if ((temp >= 33) && (temp <= 64)){
-//         temp = (((temp - 33 + offset) % 26) + 33);
-// 		}
-// 		encrypt += String.fromCharCode(temp);
-//     }
-//     return encrypt;
-//   },
-
-//   decode: (offset, texto)=> {
-//     let encrypt="";
-//     let temp = 0;
-//     
-  
-//     //Repasa el string caracter por caracter
-//     for (let i=0; i<texto.length; i++){
-//       temp=texto.charCodeAt(i);
-//       if ((temp >= 65) && (temp <= 90)){
-//         temp = (((temp - 90 - offset) % 26) + 90);
-//       }
-//       // Formula para los cambios de posición hacia la derecha de letras minúsculas
-//       else if ((temp >= 97) && (temp <= 122)){
-//         temp = (((temp - 122 - offset) % 26) + 122);
-//       }
-//       // Agregar espacio: el caracter 32 en ASCII es equivalente a espacio
-//       else if ((temp >= 33) && (temp <= 64)){
-//           temp = (((temp - 64 - offset) % 26) + 64);
-//       }
-//       encrypt += String.fromCharCode(temp);
-//       }
-//       return encrypt;
-//   }
-   
-// };
-
 window.cipher = {
   encode: (offset, string)=> {
+    //Variable vacía para contener el string final ya cifrado
     let cipherStr="";
+    //Variable en 0 para depositar el string convertido a números ASCII
     let asciiStr = 0;
-    
-
     //Repasa el string caracter por caracter
     for (let i=0; i<string.length; i++){
-		asciiStr=string.charCodeAt(i);
-		if ((asciiStr >= 65) && (asciiStr <= 90)){
-			asciiStr = (((asciiStr - 65 + offset) % 26) + 65);
-		}
-		// Formula para los cambios de posición hacia la derecha de letras minúsculas
-		else if ((asciiStr >= 97) && (asciiStr <= 122)){
-			asciiStr = (((asciiStr - 97 + offset) % 26) + 97);
-		}
-    // Agregar espacio: el caracter 32 en ASCII es equivalente a espacio
-		else if ((asciiStr >= 32) && (asciiStr <= 64)){
-        asciiStr = (((asciiStr - 32 + offset) % 33) + 32);
-		}
+      //Convertir caracteres a números ASCII  
+      asciiStr=string.charCodeAt(i);
+      //Fórmula y condicionales para codificar letras mayúsculas
+      if ((asciiStr >= 65) && (asciiStr <= 90)){
+        asciiStr = (((asciiStr - 65 + offset) % 26) + 65);
+      }
+      // Formula y condicionales para codificar letras minúsculas
+      else if ((asciiStr >= 97) && (asciiStr <= 122)){
+        asciiStr = (((asciiStr - 97 + offset) % 26) + 97);
+      }
+      // Formula y condicionales para codificar caracteres especiales
+      else {
+          asciiStr = (((asciiStr - 32 + offset) % 33) + 32);
+      }
+    //Los resultados del cifrado se depositan en la variable
+    //inicial, de numeros ASCII a caracteres
 		cipherStr += String.fromCharCode(asciiStr);
     }
+    //Devuelve la secuencia de caracteres
     return cipherStr;
   },
 
   decode: (offset, string)=> {
+    //Variable vacía para contener el string final ya descifrado
     let cipherStr="";
+    //Variable en 0 para depositar el string convertido a números ASCII
     let asciiStr = 0;
-    
-  
     //Repasa el string caracter por caracter
     for (let i=0; i<string.length; i++){
+      //Convertir caracteres a números ASCII
       asciiStr=string.charCodeAt(i);
+      //Fórmula y condicionales para codificar letras mayúsculas
       if ((asciiStr >= 65) && (asciiStr <= 90)){
         asciiStr = (((asciiStr - 90 - offset) % 26) + 90);
       }
-      // Formula para los cambios de posición hacia la derecha de letras minúsculas
+      // Formula y condicionales para codificar letras minúsculas
       else if ((asciiStr >= 97) && (asciiStr <= 122)){
         asciiStr = (((asciiStr - 122 - offset) % 26) + 122);
       }
-      // Agregar espacio: el caracter 32 en ASCII es equivalente a espacio
-      else if ((asciiStr >= 32) && (asciiStr <= 64)){
+      // Formula y condicionales para codificar caracteres especiales
+      else {
           asciiStr = (((asciiStr - 64 - offset) % 33) + 64);
       }
-      cipherStr += String.fromCharCode(asciiStr);
-      }
-      return cipherStr;
-  }
-   
+    //Los resultados del cifrado se depositan en la variable
+    //inicial, de numeros ASCII a caracteres
+    cipherStr += String.fromCharCode(asciiStr);
+    }
+    return cipherStr;
+  }  
 };
-
+//test encode
+let string = "Time and location";
+let encStr = window.cipher.encode(2,string);
+console.log(encStr);
+//test decode
+let dcodStr = window.cipher.decode(2,encStr);
+console.log(dcodStr);
 
